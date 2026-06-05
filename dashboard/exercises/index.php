bcd2f4847b1d9f8a7e8714b06acf238747585ae6
dimsoumis <101522718+dimsoumis@users.noreply.github.com>
2026-06-05 15:47:39 +0300

    Add files via upload

---
+++ dashboard/exercises/index.php
@@ -0,0 +1,31 @@
+
+<?php include '../loggedin-head.php';?>
+
+<main>
+	
+	<h1 id="pageTitle">Exercises</h1>
+	
+<p style="text-align: center;">Choose the exercises and the assessment tests that best meet your needs!</p>
+	
+	<div class="row">
+			<div class="column3">
+			<a href="fine-motor-skills/index.php"><img src="/images/fine-motor-skills.png" /></a>
+				<a href="fine-motor-skills/index.php"><h3>Fine Motor Skills</h3></a>
+	</div>
+		<div class="column3">
+			<a href="hand-exercises/index.php"><img src="/images/hand-exercises.png" /></a>
+				<a href="hand-exercises/index.php"><h3>Upper-Limbs Gross Dexterity</h3></a>
+	</div>
+		<div class="column3">
+			<a href="leg-exercises/index.php"><img src="/images/legs-and-balance.png" /></a>
+				<a href="leg-exercises/index.php"><h3>Lower-Limbs & Balance</h3></a>
+	</div>
+	
+	
+	</div>
+	
+
+	
+</main>
+
+<?php include '../../main-footer.php';?>
---
+++ dashboard/previous-scores/index.php
@@ -0,0 +1,97 @@
+
+<?php include '../loggedin-head.php';?>
+
+<main>
+	
+	<h1 id="pageTitle">Previous Scores</h1>
+	
+<p style="text-align: center; margin-bottom: 75px;">Choose the exercises in which you want to check your previous performances.</p>
+	
+		<div class="row">
+			<div class="column4" style="justify-content: center;">
+			<h2>Fine Motor Skills</h2>
+		</div>
+		<div class="column4">
+			<a href="/dashboard/exercises/fine-motor-skills/bbt/previous-scores.php"><img style="width: 350px;" src="/images/box-and-blocks.png" /></a>
+				<a href="/dashboard/exercises/fine-motor-skills/bbt/previous-scores.php"><h3 style="text-align: center;">Box & Blocks</h3></a>
+			
+			
+			<a href="/dashboard/exercises/fine-motor-skills/bbt/previous-scores.php"><button style="margin: 0px 0px 10px 0px;">Check Scores</button></a>
+		
+	</div>
+		<div class="column4">
+					<a href="/dashboard/exercises/fine-motor-skills/categorize/previous-scores.php"><img style="width: 350px;" src="/images/categorize.png" /></a>
+				<a href="/dashboard/exercises/fine-motor-skills/categorize/previous-scores.php"><h3 style="text-align: center;">Categorize</h3></a>
+				<a href="/dashboard/exercises/fine-motor-skills/categorize/previous-scores.php"><button style="margin: 0px 0px 10px 0px;">Check Scores</button></a>
+			
+	</div>
+			<div class="column4">
+			&nbsp;
+		</div>
+	
+
+	</div>
+	
+	<hr>
+	<div class="row">
+		
+		<div class="column4" style="justify-content: center;">
+			<h2>Upper-Limbs</h2>
+		</div>
+		<div class="column4">
+			<a href="/dashboard/exercises/hand-exercises/bicep-curls/previous-scores.php"><img src="/images/bicep-curls.png" /></a>
+				<a href="/dashboard/exercises/hand-exercises/bicep-curls/previous-scores.php"><h3 style="text-align: center;">Bicep curls</h3></a>
+			<a href="/dashboard/exercises/hand-exercises/bicep-curls/previous-scores.php"><button style="margin: 0px 0px 10px 0px;">Check Scores</button></a>
+		
+	</div>
+		<div class="column4">
+			<a href="/dashboard/exercises/hand-exercises/overhead-press/previous-scores.php"><img src="/images/overhead-press.png" /></a>
+				<a href="/dashboard/exercises/hand-exercises/overhead-press/previous-scores.php"><h3 style="text-align: center;">Overhead press</h3></a>
+				<a href="/dashboard/exercises/hand-exercises/overhead-press/previous-scores.php"><button style="margin: 0px 0px 10px 0px;">Check Scores</button></a>
+		
+	</div>
+			<div class="column4">
+			<a href="/dashboard/exercises/hand-exercises/lateral-raises/previous-scores.php"><img src="/images/lateral-raises.png" /></a>
+				<a href="/dashboard/exercises/hand-exercises/lateral-raises/previous-scores.php"><h3 style="text-align: center;">Lateral raises</h3></a>
+					<a href="/dashboard/exercises/hand-exercises/lateral-raises/previous-scores.php"><button style="margin: 0px 0px 10px 0px;">Check Scores</button></a>
+		
+	</div>
+	
+
+	</div>
+	
+			
+<hr>
+	
+	<div class="row">
+			<div class="column4" style="justify-content: center;">
+			<h2>Lower-Limbs</h2>
+		</div>
+		<div class="column4">
+			<a href="/dashboard/exercises/leg-exercises/sit-to-stand/previous-scores.php"><img style="width: 350px;" src="/images/sit-to-stand.png" /></a>
+				<a href="/dashboard/exercises/leg-exercises/sit-to-stand/previous-scores.php"><h3 style="text-align: center;">Sit to Stand</h3></a>
+			
+			
+			<a href="/dashboard/exercises/leg-exercises/sit-to-stand/previous-scores.php"><button style="margin: 0px 0px 10px 0px;">Check Scores</button></a>
+		
+	</div>
+		<div class="column4">
+					<a href="/dashboard/exercises/leg-exercises/1-foot-balance/previous-scores.php"><img style="width: 350px;" src="/images/legs-and-balance.png" /></a>
+				<a href="/dashboard/exercises/leg-exercises/1-foot-balance/previous-scores.php"><h3 style="text-align: center;">1 Foot Balance</h3></a>
+				<a href="/dashboard/exercises/leg-exercises/1-foot-balance/previous-scores.php"><button style="margin: 0px 0px 10px 0px;">Check Scores</button></a>
+			
+	</div>
+			<div class="column4">
+			&nbsp;
+		</div>
+	
+
+	</div>
+	
+</main>
+
+
+
+<?php include '../../main-footer.php';?>
+
+
